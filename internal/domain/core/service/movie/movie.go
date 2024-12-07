17a0d0c34fde6fb37ad2dc93e447336039599912
Kaan Kuscu <me@kaanksc.com>
2024-12-07 16:20:09 +0300

    add movie service

--- internal/domain/core/service/movie/movie.go
+++ internal/domain/core/service/movie/movie.go
@@ -1,7 +1,72 @@
 package movie
 
-type Movie struct{}
+import (
+	"context"
+	"fmt"
+	"mediaserver/internal/customerrors"
+	"mediaserver/internal/domain/core/model"
+	"mediaserver/internal/port"
+	"time"
+)
 
-func New() (*Movie, error) {
-	return &Movie{}, nil
+type MovieRepository interface {
+	CreateMovie(ctx context.Context, movie *model.Movie) error
+	GetMovieByID(ctx context.Context, id string) (*model.Movie, error)
+}
+
+type Movie struct {
+	movieRepository MovieRepository
+	idgen           port.IDGenerator
+	logger          port.Logger
+}
+
+func New(movieRepository MovieRepository, idgen port.IDGenerator, lg port.Logger) (*Movie, error) {
+	return &Movie{
+		movieRepository: movieRepository,
+		idgen:           idgen,
+		logger:          lg,
+	}, nil
+}
+
+func (m *Movie) CreateMovie(ctx context.Context, movie *model.Movie) (string, error) {
+	if movie.Title == "" {
+		err := customerrors.ErrInvalidTitle
+		m.logger.Error(ctx, "error when creating movie",
+			"error", err,
+		)
+		return "", err
+	}
+
+	movie.ID = m.idgen.NewID()
+	movie.CreatedAt = time.Now()
+
+	err := m.movieRepository.CreateMovie(ctx, movie)
+	if err != nil {
+		err = fmt.Errorf("movieRepository.CreateMovie: %w", err)
+		m.logger.Error(ctx, "error when creating movie",
+			"error", err,
+		)
+		return "", err
+	}
+	m.logger.Info(ctx, "movie created",
+		"id", movie.ID, "title", movie.Title,
+	)
+	return movie.ID, nil
+}
+
+func (m *Movie) GetMovieByID(ctx context.Context, id string) (*model.Movie, error) {
+	movie, err := m.movieRepository.GetMovieByID(ctx, id)
+	if err != nil {
+		err = fmt.Errorf("movieRepository.GetMovieByID: %w", err)
+		m.logger.Error(ctx, "error when getting movie",
+			"error", err,
+			"id", id,
+		)
+		return nil, err
+	}
+	m.logger.Info(ctx, "movie retrieved",
+		"id", movie.ID,
+		"title", movie.Title,
+	)
+	return movie, nil
 }
