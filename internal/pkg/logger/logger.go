ee4f75665a7a7103c812e9a2b8ab8b0b23ecbbd4
Kaan Kuscu <me@kaanksc.com>
2024-12-07 16:18:14 +0300

    add logger

---
+++ internal/pkg/logger/logger.go
@@ -0,0 +1,52 @@
+package logger
+
+import (
+	"context"
+	"io"
+	"mediaserver/internal/port"
+
+	"github.com/rs/zerolog"
+)
+
+var _ port.Logger = (*Logger)(nil)
+
+type Logger struct {
+	zlog *zerolog.Logger
+}
+
+func New() (*Logger, error) {
+	cw := zerolog.NewConsoleWriter()
+	mw := io.MultiWriter(cw)
+	zlog := zerolog.New(mw).With().Timestamp().CallerWithSkipFrameCount(3).Logger()
+	return &Logger{
+		zlog: &zlog,
+	}, nil
+}
+
+func (l *Logger) Trace(ctx context.Context, message string, fields ...any) {
+	l.zlog.Trace().Fields(fields).Msg(message)
+}
+
+func (l *Logger) Debug(ctx context.Context, message string, fields ...any) {
+	l.zlog.Debug().Fields(fields).Msg(message)
+}
+
+func (l *Logger) Info(ctx context.Context, message string, fields ...any) {
+	l.zlog.Info().Fields(fields).Msg(message)
+}
+
+func (l *Logger) Warn(ctx context.Context, message string, fields ...any) {
+	l.zlog.Warn().Fields(fields).Msg(message)
+}
+
+func (l *Logger) Error(ctx context.Context, message string, fields ...any) {
+	l.zlog.Error().Fields(fields).Msg(message)
+}
+
+func (l *Logger) Fatal(ctx context.Context, message string, fields ...any) {
+	l.zlog.Fatal().Fields(fields).Msg(message)
+}
+
+func (l *Logger) Panic(ctx context.Context, message string, fields ...any) {
+	l.zlog.Panic().Fields(fields).Msg(message)
+}
