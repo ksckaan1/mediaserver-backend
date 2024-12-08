1902337daf0524c50519c1b9f0704c38b864f9f0
Kaan Kuscu <me@kaanksc.com>
2024-12-08 23:18:13 +0300

    add error type for ErrUnexpectedStatusCode

--- internal/customerrors/customerrors.go
+++ internal/customerrors/customerrors.go
@@ -1,8 +1,19 @@
 package customerrors
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	ErrInvalidTitle   = errors.New("invalid title")
 	ErrRecordNotFound = errors.New("record not found")
 )
+
+type ErrUnexpectedStatusCode struct {
+	StatusCode int
+}
+
+func (e ErrUnexpectedStatusCode) Error() string {
+	return fmt.Sprintf("unexpected status code: %d", e.StatusCode)
+}
