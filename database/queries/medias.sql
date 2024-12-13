95ddc46cc555081fe93cb5255a087eb517187dc2
Kaan Kuscu <me@kaanksc.com>
2024-12-13 23:40:41 +0300

    add sql queries for media table

---
+++ database/queries/medias.sql
@@ -0,0 +1,13 @@
+-- name: CreateMedia :exec
+INSERT INTO medias (id, path, type, storage_type, size, created_at)
+		VALUES(?, ?, ?, ?, ?, (datetime (CURRENT_TIMESTAMP, 'localtime')));
+
+-- name: GetMediaByID :one
+SELECT *
+FROM medias
+WHERE id = ?;
+
+-- name: DeleteMediaByID :one
+DELETE FROM medias
+WHERE id = ?
+RETURNING id;
