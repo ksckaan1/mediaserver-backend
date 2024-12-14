0666038ea579edb13fda3089b5011fab08d6486c
Kaan Kuscu <me@kaanksc.com>
2024-12-15 01:25:15 +0300

    add custom error types

--- internal/customerrors/customerrors.go
+++ internal/customerrors/customerrors.go
@@ -6,9 +6,11 @@ import (
 )
 
 var (
-	ErrInvalidTitle        = errors.New("invalid title")
 	ErrRecordNotFound      = errors.New("record not found")
+	ErrMovieNotFound       = errors.New("movie not found")
+	ErrMediaNotFound       = errors.New("media not found")
 	ErrUnsupportedFileType = errors.New("unsupported file type")
+	ErrInvalidMediaType    = errors.New("invalid media type")
 )
 
 type ErrUnexpectedStatusCode struct {
