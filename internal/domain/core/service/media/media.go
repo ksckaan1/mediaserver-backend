60a5edc6a92c7e78ae912d91059f887816a0eadd
Kaan Kuscu <me@kaanksc.com>
2024-12-18 21:55:09 +0300

    add Get, List, Delete Media funcs for media service

--- internal/domain/core/service/media/media.go
+++ internal/domain/core/service/media/media.go
@@ -9,7 +9,11 @@ import (
 
 type Repository interface {
 	CreateMedia(ctx context.Context, media *model.Media) error
+	GetMediaByID(ctx context.Context, id string) (*model.Media, error)
+	ListMedias(ctx context.Context, limit, offset int64) (*model.MediaList, error)
+	DeleteMediaByID(ctx context.Context, id string) error
 }
+
 type Media struct {
 	repo   Repository
 	idgen  port.IDGenerator
@@ -41,5 +45,55 @@ func (m *Media) Create(ctx context.Context, fi *model.FileInfo) (string, error) 
 		)
 		return "", err
 	}
+	m.logger.Info(ctx, "media created",
+		"id", id,
+	)
 	return id, nil
+}
+
+func (m *Media) GetMediaByID(ctx context.Context, id string) (*model.Media, error) {
+	media, err := m.repo.GetMediaByID(ctx, id)
+	if err != nil {
+		err = fmt.Errorf("repo.GetMediaByID: %w", err)
+		m.logger.Error(ctx, "error when getting media",
+			"error", err,
+		)
+		return nil, err
+	}
+	m.logger.Info(ctx, "media retrieved",
+		"id", media.ID,
+	)
+	return media, nil
+}
+
+func (m *Media) ListMedias(ctx context.Context, limit, offset int64) (*model.MediaList, error) {
+	list, err := m.repo.ListMedias(ctx, limit, offset)
+	if err != nil {
+		err = fmt.Errorf("repo.ListMedias: %w", err)
+		m.logger.Error(ctx, "error when listing medias",
+			"error", err,
+		)
+		return nil, err
+	}
+	m.logger.Info(ctx, "medias listed",
+		"count", list.Count,
+		"limit", list.Limit,
+		"offset", list.Offset,
+	)
+	return list, nil
+}
+
+func (m *Media) DeleteMediaByID(ctx context.Context, id string) error {
+	err := m.repo.DeleteMediaByID(ctx, id)
+	if err != nil {
+		err = fmt.Errorf("repo.DeleteMediaByID: %w", err)
+		m.logger.Error(ctx, "error when deleting media",
+			"error", err,
+		)
+		return err
+	}
+	m.logger.Info(ctx, "media deleted",
+		"id", id,
+	)
+	return nil
 }
