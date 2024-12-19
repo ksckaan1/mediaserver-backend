9ec7a7ebbde0e51d8aa254452eab30f36c874d0b
Kaan Kuscu <me@kaanksc.com>
2024-12-19 23:06:36 +0300

    add support for fething series detail for tmdb client

--- internal/infrastructure/tmdbclient/tmdbclient.go
+++ internal/infrastructure/tmdbclient/tmdbclient.go
@@ -46,11 +46,48 @@ func (t *TMDBClient) GetMovieDetail(ctx context.Context, id int64) (*model.TMDBI
 		})
 	}
 
-	var result model.TMDBInfo
+	var result model.TMDBData
 	err = json.NewDecoder(resp.Body).Decode(&result)
 	if err != nil {
 		return nil, fmt.Errorf("json.NewDecoder.Decode: %w", err)
 	}
 
-	return &result, nil
+	return &model.TMDBInfo{
+		ID:   fmt.Sprintf("movie-%d", id),
+		Data: result,
+	}, nil
+}
+
+const seriesDetailURL = "https://api.themoviedb.org/3/tv/%d"
+
+func (t *TMDBClient) GetSeriesDetail(ctx context.Context, id int64) (*model.TMDBInfo, error) {
+	uri := fmt.Sprintf(seriesDetailURL, id)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, uri, http.NoBody)
+	if err != nil {
+		return nil, fmt.Errorf("http.NewRequestWithContext: %w", err)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", t.apiKey))
+
+	resp, err := t.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("client.Do: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("check status code: %w", customerrors.ErrUnexpectedStatusCode{
+			StatusCode: resp.StatusCode,
+		})
+	}
+
+	var result model.TMDBData
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, fmt.Errorf("json.NewDecoder.Decode: %w", err)
+	}
+
+	return &model.TMDBInfo{
+		ID:   fmt.Sprintf("series-%d", id),
+		Data: result,
+	}, nil
 }
