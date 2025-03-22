cb165ff0180676623b7a4817d3a282dcc70331a5
Kaan Kuscu <me@kaanksc.com>
2025-03-22 22:02:43 +0300

    add series domain model

--- series_service/internal/domain/core/models/series.go
+++ series_service/internal/domain/core/models/series.go
@@ -4,8 +4,8 @@ import "time"
 
 type Series struct {
 	ID          string    `json:"id" bson:"_id"`
-	CreatedAt   time.Time `json:"created_at" bson:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at" bson:"updated_at"`
+	CreatedAt   time.Time `json:"created_at" bson:"created_at,omitempty"`
+	UpdatedAt   time.Time `json:"updated_at" bson:"updated_at,omitempty"`
 	Title       string    `json:"title" bson:"title"`
 	Description string    `json:"description" bson:"description"`
 	TMDBID      string    `json:"tmdb_id" bson:"tmdb_id"`
