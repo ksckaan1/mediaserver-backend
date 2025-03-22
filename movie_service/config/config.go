f01dda4b4e21ac366a79af78e6062085c8e77265
Kaan Kuscu <me@kaanksc.com>
2025-03-22 03:33:52 +0300

    add configs

--- movie_service/config/config.go
+++ movie_service/config/config.go
@@ -2,13 +2,16 @@ package config
 
 import (
 	"fmt"
+
 	"github.com/caarlos0/env/v11"
 )
 
 type Config struct {
 	Port            int    `env:"PORT" envDefault:"8081"`
 	MediaServerHost string `env:"MEDIA_SERVER_HOST" envDefault:"localhost:8080"`
+	TMDBServerHost  string `env:"TMDB_SERVER_HOST" envDefault:"localhost:8082"`
 	DatabaseURL     string `env:"DATABASE_URL" envDefault:"mongodb://localhost:27017"`
+	DatabaseName    string `env:"DATABASE_NAME" envDefault:"movie_service"`
 }
 
 func New() *Config {
