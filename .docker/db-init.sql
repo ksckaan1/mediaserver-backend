2d4c1aa2e0053cdb0c923768368d87b586725b99
ksckaan1 <me@kaanksc.com>
2025-03-27 01:49:09 +0300

    remove old init scripts

--- .docker/db-init.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE mediaservice;
-CREATE USER mediaservice WITH PASSWORD 'mediaservice';
-GRANT ALL PRIVILEGES ON DATABASE mediaservice TO mediaservice;
-\connect mediaservice
-ALTER DATABASE mediaservice OWNER TO mediaservice;
-ALTER SCHEMA public OWNER TO mediaservice;
