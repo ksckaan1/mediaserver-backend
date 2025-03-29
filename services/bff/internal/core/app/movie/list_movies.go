45c78b490da5e4090de20b7d479f107ff489c0bc
ksckaan1 <me@kaanksc.com>
2025-03-29 20:31:10 +0300

    add movie app for bff

---
+++ services/bff/internal/core/app/movie/list_movies.go
@@ -0,0 +1,83 @@
+package movie
+
+import (
+	"bff-service/internal/core/models"
+	"context"
+	"fmt"
+	"net/http"
+	"shared/enums/mediatype"
+	"shared/pb/moviepb"
+
+	"github.com/samber/lo"
+)
+
+type ListMovies struct {
+	movieClient moviepb.MovieServiceClient
+}
+
+func NewListMovies(movieClient moviepb.MovieServiceClient) *ListMovies {
+	return &ListMovies{
+		movieClient: movieClient,
+	}
+}
+
+type ListMoviesRequest struct {
+	Limit  int64 `query:"limit"`
+	Offset int64 `query:"offset"`
+}
+
+type ListMoviesResponse struct {
+	List   []*models.Movie `json:"list"`
+	Count  int64           `json:"count"`
+	Limit  int64           `json:"limit"`
+	Offset int64           `json:"offset"`
+}
+
+func (h *ListMovies) Handle(ctx context.Context, req *ListMoviesRequest) (*ListMoviesResponse, int, error) {
+	resp, err := h.movieClient.ListMovies(ctx, &moviepb.ListMoviesRequest{
+		Limit:  req.Limit,
+		Offset: req.Offset,
+	})
+	if err != nil {
+		return nil, http.StatusInternalServerError, fmt.Errorf("movieClient.ListMovies: %w", err)
+	}
+
+	return &ListMoviesResponse{
+		List: lo.Map(resp.List, func(movie *moviepb.Movie, _ int) *models.Movie {
+			var mediaInfo *models.Media
+			if movie.MediaInfo != nil {
+				mediaInfo = &models.Media{
+					ID:        movie.MediaInfo.Id,
+					CreatedAt: movie.MediaInfo.CreatedAt.AsTime(),
+					UpdatedAt: movie.MediaInfo.UpdatedAt.AsTime(),
+					Title:     movie.MediaInfo.Title,
+					Path:      movie.MediaInfo.Path,
+					MimeType:  movie.MediaInfo.MimeType,
+					Size:      movie.MediaInfo.Size,
+					Type:      mediatype.FromNumber(int32(movie.MediaInfo.Type)),
+				}
+			}
+
+			var tmdbInfo *models.TMDB
+			if movie.TmdbInfo != nil {
+				tmdbInfo = &models.TMDB{
+					Id:   movie.TmdbInfo.Id,
+					Data: movie.TmdbInfo.Data.AsMap(),
+				}
+			}
+
+			return &models.Movie{
+				ID:          movie.Id,
+				CreatedAt:   movie.CreatedAt.AsTime(),
+				UpdatedAt:   movie.UpdatedAt.AsTime(),
+				Title:       movie.Title,
+				Description: movie.Description,
+				MediaInfo:   mediaInfo,
+				TmdbInfo:    tmdbInfo,
+			}
+		}),
+		Count:  resp.Count,
+		Limit:  req.Limit,
+		Offset: req.Offset,
+	}, http.StatusOK, nil
+}
