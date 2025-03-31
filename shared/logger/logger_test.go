ed9b67afdf899b8e72140ad4c7128a1718028611
ksckaan1 <me@kaanksc.com>
2025-03-31 17:59:48 +0300

    update logger package

--- shared/logger/logger_test.go
+++ shared/logger/logger_test.go
@@ -3,13 +3,10 @@ package logger
 import (
 	"context"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 func TestLogger(t *testing.T) {
-	logger, err := New()
-	require.NoError(t, err)
+	l := New()
 
-	logger.Info(context.TODO(), "test", "key1", "field1", "key2", 42)
+	l.Info(context.TODO(), "test", "key1", "field1", "key2", 42)
 }
