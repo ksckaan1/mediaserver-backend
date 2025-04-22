3f94ee90fe2758e1f2ba784b69af303d844a6334
Kaan Kuscu <me@kaanksc.com>
2025-04-22 15:56:57 +0300

    add user type mw

--- services/bff/cmd/api/main.go
+++ services/bff/cmd/api/main.go
@@ -20,14 +20,14 @@ func main() {
 func initializer(ctx context.Context, s *service.RESTService[config.Config]) error {
 	s.Router.Use(requestIDMW(s.IDGenerator))
 	v1 := s.Router.Group("/api/v1")
-	authRoutes, authMW := initAuthRoutes(s.ServiceClients.AuthServiceClient)
+	authRoutes, authMW, userTypeMW := initAuthRoutes(s.ServiceClients.AuthServiceClient, s.ServiceClients.UserServiceClient)
 	v1.Mount("/auth", authRoutes)
-	v1.Mount("/user", initUserRoutes(s.ServiceClients.UserServiceClient, authMW))
+	v1.Mount("/user", initUserRoutes(s.ServiceClients.UserServiceClient, authMW, userTypeMW))
 	v1.Use(authMW)
-	v1.Mount("/media", initMediaRoutes(s.ServiceClients.MediaServiceClient))
-	v1.Mount("/movie", initMovieRoutes(s.ServiceClients.MovieServiceClient))
-	v1.Mount("/series", initSeriesRoutes(s.ServiceClients.SeriesServiceClient))
-	v1.Mount("/season", initSeasonRoutes(s.ServiceClients.SeasonServiceClient))
-	v1.Mount("/episode", initEpisodeRoutes(s.ServiceClients.EpisodeServiceClient))
+	v1.Mount("/media", initMediaRoutes(s.ServiceClients.MediaServiceClient, userTypeMW))
+	v1.Mount("/movie", initMovieRoutes(s.ServiceClients.MovieServiceClient, userTypeMW))
+	v1.Mount("/series", initSeriesRoutes(s.ServiceClients.SeriesServiceClient, userTypeMW))
+	v1.Mount("/season", initSeasonRoutes(s.ServiceClients.SeasonServiceClient, userTypeMW))
+	v1.Mount("/episode", initEpisodeRoutes(s.ServiceClients.EpisodeServiceClient, userTypeMW))
 	return nil
 }
